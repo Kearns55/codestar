4b69f87f8628fa1952389daf31f03ced055ed5b4
Kearns55 <rebekah120@hotmail.com>
2025-11-06 14:15:22 +0100

    Added database

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_JPLCOA2byfx3@ep-falling-leaf-agfywzze.c-2.eu-central-1.aws.neon.tech/pod_truck_diner_701008") 
