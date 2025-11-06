b87feb368f300082b2bbd09e17cce9de4b504aca
Kearns55 <rebekah120@hotmail.com>
2025-11-06 14:32:42 +0100

    Connect database

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-27brx6+x_v0k1*=&2k9tr%hnv=ryg)wlh_(ga=99f*108m#^*t'
+SECRET_KEY = 'os.environ.get("SECRET_KEY")'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['codestar99-9541b3a07f4f.herokuapp.com', 'localhost', '127.0.0.1']
 
--- env.py
+++ env.py
@@ -2,3 +2,8 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_JPLCOA2byfx3@ep-falling-leaf-agfywzze.c-2.eu-central-1.aws.neon.tech/pod_truck_diner_701008") 
+
+
+os.environ.setdefault(
+    'SECRET_KEY', '6560c0c1b8516bd122d65548fb861d63'
+)
