f7e24598bb93f92fdc4297504b08dd22845dc971
Kearns55 <rebekah120@hotmail.com>
2025-11-07 10:12:01 +0100

    Add generic post view

--- blog/models.py
+++ blog/models.py
@@ -8,8 +8,28 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="blog_posts"
-)
-content = models.TextField()
-created_on = models.DateTimeField(auto_now_add=True)
-status = models.IntegerField(choices=STATUS, default=0)
+    User, on_delete=models.CASCADE, related_name="blog_posts")
+    content = models.TextField(default='')
+    created_on = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.CharField(max_length=255, blank=True, null=True, default='')
+    updates_on = models.DateTimeField(auto_now=True, null=True)
+    class Meta:
+        ordering = ["-created_on"]
+    def __str__(self):
+        return f"The title of this post is {self.title} | written by {self.author}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ["created_on"]
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
+
