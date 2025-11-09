ddcbfe13909f1e4e5452da8e0787d58f5da2bb5e
Kearns55 <rebekah120@hotmail.com>
2025-11-09 14:20:17 +0100

    Add pagination

--- blog/models.py
+++ blog/models.py
@@ -5,15 +5,15 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
+    title = models.CharField(max_length=300, unique=True)
+    slug = models.SlugField(max_length=300, unique=True)
     author = models.ForeignKey(
     User, on_delete=models.CASCADE, related_name="blog_posts")
     content = models.TextField(default='')
     created_on = models.DateTimeField(auto_now_add=True, null=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    excerpt = models.CharField(max_length=255, blank=True, null=True, default='')
-    updates_on = models.DateTimeField(auto_now=True, null=True)
+    excerpt = models.CharField(max_length=300, blank=True, null=True, default='')
+    updated_on = models.DateTimeField(auto_now=True, null=True)
     class Meta:
         ordering = ["-created_on"]
     def __str__(self):
